e4001db577ea5526c753dc9b22c06cc28e97c940
parvez <parvezyesrat17032024@gmail.com>
2025-09-12 21:20:37 +0600

    added functionality

--- index.js
+++ index.js
@@ -1,30 +1,56 @@
-document.getElementById('all-tree')
-  .addEventListener('click', () => {
-    fetch("https://openapi.programming-hero.com/api/plants")
-      .then(res => res.json())
-      .then(json => {
-        const plants = json.plants;
-       const plantContainer=document.getElementById('plant-trees');
-        plants.forEach(plant => {
-          const div =document.createElement('div');
-          div.innerHTML=`
-<div class="border-gray-400 shadow-md bg-white p-2 rounded-lg h-[400px] flex flex-col">
-  <img src="${plant.image}" alt="" class="h-45 w-full object-cover rounded-md">
-  <div class="">
-  <h1 class="font-bold">${plant.name}</h1>
-  <p class="flex-grow">${plant.description}</p>
-  <div class="flex justify-between items-center p-2">
-    <h1 class="text-[#15803D] bg-[#DCFCE7] rounded-xl p-2">${plant.category}</h1>
-    <h1 class="font-bold">
-      <i class="fa-sharp fa-solid fa-bangladeshi-taka-sign"></i>${plant.price}
-    </h1>
-  </div>
-  <a class="btn bg-[#15803D] text-white mt-2 rounded-3xl w-full">Add to Cart</a>
-  </div>
-</div>
+//load All  Tree
+const loadAllPlants = () => {
+  fetch("https://openapi.programming-hero.com/api/plants")
+    .then(res => res.json())
+    .then(json => displayTrees(json.plants));
+};
 
+//Display All Trees 
+const displayTrees = plants => {
+  const plantContainer = document.getElementById('plant-trees');
+  plants.forEach(plant => {
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="border-gray-400 shadow-md bg-white p-3 rounded-lg h-[450px] flex flex-col">
+          <img src="${plant.image}" alt="" class="h-45 w-full object-cover rounded-md">     
+          <div class="flex flex-col flex-grow">
+              <h1 class="font-bold">${plant.name}</h1>
+              <p class="flex-grow">${plant.description}</p>
+              <div class="flex justify-between items-center p-2">
+                  <h1 class="text-[#15803D] bg-[#DCFCE7] rounded-xl p-2">${plant.category}</h1>
+                  <h1 class="font-bold"><i class="fa-sharp fa-solid fa-bangladeshi-taka-sign"></i>${plant.price}</h1>
+              </div>     
+                <!-- Button at bottom -->
+              <a id="${plant.id}" class="btn bg-[#15803D] text-white mt-auto rounded-3xl w-full">Add to Cart</a>
+          </div>
+    </div>
           `
-          plantContainer.appendChild(div);
-        })
-      })
-  })
+    plantContainer.appendChild(div);
+
+  });
+};
+loadAllPlants();
+
+//load all categories
+const loadAllCategories = () => {
+  fetch("https://openapi.programming-hero.com/api/categories")
+    .then(res => res.json())
+    .then(json => displayAllCategories(json.categories));
+};
+
+//display all categories
+const displayAllCategories = categories => {
+  const categoryContainer = document.getElementById('all-categories');
+  categoryContainer.innerHTML = "";
+  categories.forEach(category => {
+    const li = document.createElement('li');
+    li.innerHTML += `
+    <div id="${category.id}" class="text-xl mb-2">${category.category_name}</div>
+    `
+    categoryContainer.appendChild(li);
+    // loadCategory(category.id);
+    // console.log(category.category_name)
+  });
+};
+loadAllCategories();
+
